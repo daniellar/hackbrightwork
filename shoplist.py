c2683d8d645e35dd486a3ad5b1895392efa382d6
Hackbright Student <info@hackbrightacademy.com>
2016-08-02 21:26:05 -0700

    shoplist

---
+++ shoplist.py
@@ -0,0 +1,23 @@
+shoplist = [None]
+
+def main():
+    choice = raw_input("Do you want to add or remove something to the list?").lower
+    if choice == "add":
+        pass
+            #delete pass, it's just a placeholder so the code doesn't break
+            #here make the code ask what they want to add to the shopping list, and run
+    else:
+        pass
+            #again delete pass
+            #get it to ask what you want to remove, and run the remove function
+
+
+
+# def add_item(item):
+#     item = raw_input("Add item to shopping list.").lower()
+#     shoplist.append(item)
+   
+# print shoplist
+
+if __name__ == '__main__':
+    main()
