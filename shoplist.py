43975a2a4a9b847999ae8e364ec97d9edcd6b76d
amyclaussen <aeclaussen@gmail.com>
2016-08-02 23:23:57 -0700

    working script

--- shoplist.py
+++ shoplist.py
@@ -1,23 +1,34 @@
-shoplist = [None]
+shoplist = []
+
+
+def add_item(item):
+    newshoplist = shoplist.append(item)
+    return newshoplist
+
+
+def remove_item(item):
+    newshoplist = shoplist.remove(item)
+    return newshoplist
+   
 
 def main():
-    choice = raw_input("Do you want to add or remove something to the list?").lower
-    if choice == "add":
-        pass
-            #delete pass, it's just a placeholder so the code doesn't break
-            #here make the code ask what they want to add to the shopping list, and run
-    else:
-        pass
-            #again delete pass
-            #get it to ask what you want to remove, and run the remove function
-
-
-
-# def add_item(item):
-#     item = raw_input("Add item to shopping list.").lower()
-#     shoplist.append(item)
-   
-# print shoplist
+    while(True):
+        choice = raw_input("Do you want to add or remove something to the list? ").lower()
+        if choice == "add":
+            item = raw_input("Which item would you like to add? ").lower()
+            if item in shoplist:
+                print "This item is already in the list."
+            else:
+                add_item(item)
+        elif choice == "remove":
+            item = raw_input("Which item would you like to remove? ").lower()
+            if item not in shoplist:
+                print "This item is not in the list."
+            else:
+                remove_item(item)
+        else:
+            print "Only add or remove were options."
+        print "Your current shopping list is:", shoplist
 
 if __name__ == '__main__':
     main()
