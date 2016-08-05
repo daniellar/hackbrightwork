34b0e368121246a1b34102f0a096f2df9d01c200
amyclaussen <aeclaussen@gmail.com>
2016-08-04 17:36:46 -0700

    revision of shoplist where raw input part of smaller functions instead of main function

--- shoplist.py
+++ shoplist.py
@@ -1,31 +1,26 @@
-shoplist = []
+def add_item(shoplist):
+    item = raw_input("Which item would you like to add? ").lower()
+    if item in shoplist:
+        print "This item is already in the list."
+    else:
+        shoplist.append(item)
 
-
-def add_item(item):
-    newshoplist = shoplist.append(item)
-    return newshoplist
-
-
-def remove_item(item):
-    newshoplist = shoplist.remove(item)
-    return newshoplist
-   
+def remove_item(shoplist):
+    item = raw_input("Which item would you like to remove? ").lower()
+    if item not in shoplist:
+        print "This item is not in the list."
+    else:
+        shoplist.remove(item)
 
 def main():
-    while(True):
-        choice = raw_input("Do you want to add or remove something to the list? ").lower()
+    shoplist = []
+    choice = None
+    while choice != "quit":
+        choice = raw_input("Do you want to add or remove something to the list? (Type 'quit' to exit.) ").lower()
         if choice == "add":
-            item = raw_input("Which item would you like to add? ").lower()
-            if item in shoplist:
-                print "This item is already in the list."
-            else:
-                add_item(item)
+            add_item(shoplist)
         elif choice == "remove":
-            item = raw_input("Which item would you like to remove? ").lower()
-            if item not in shoplist:
-                print "This item is not in the list."
-            else:
-                remove_item(item)
+            remove_item(shoplist)
         else:
             print "Only add or remove were options."
         print "Your current shopping list is:", shoplist
